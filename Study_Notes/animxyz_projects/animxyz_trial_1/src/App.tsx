d309a0f268c28ce73bbc3c92729880b8d1e9d226
tieje <toj320@gmail.com>
2021-10-01 11:57:29 -0400

    halfway through fr the day

--- Study_Notes/animxyz_projects/animxyz_trial_1/src/App.tsx
+++ Study_Notes/animxyz_projects/animxyz_trial_1/src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { XyzTransition } from "@animxyz/react";
 
 function App() {
+  const [state, setState] = useState(false)
+  const fadeNOut = () => {
+    if (state) {
+      setState(false)
+    }
+    else {
+      setState(true)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <XyzTransition appear xyz="fade rotate-right ease-out-back">
+          {state && <div className="square" />}
+        </XyzTransition>
+        <button onClick={fadeNOut}>fade toggle</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ Study_Notes/d3_projects/d3_trial_1/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Svg/>
+  )
+}
+
+const Svg = () => {
+  return (
+    <svg style={{
+      border: "2px solid gold"
+    }} />
+)}
+export default App;
+
