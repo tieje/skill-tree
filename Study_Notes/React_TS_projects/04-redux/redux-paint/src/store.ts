47c39205068e30090f2c7fb182f0632ca3c520d7
tieje <toj320@gmail.com>
2021-10-04 23:09:47 -0400

    I will no longer be using apollo, graphql, express, python, or django for this project. Using faunadb really automates away a lot of work. I don't need to set up an ORM or db or REST API. It's pretty cool. I think I'll pick up tailwind css and redux. I didn't get to all of learning goals for today, but I definitely will tomorrow.

---
+++ Study_Notes/React_TS_projects/04-redux/redux-paint/src/store.ts
@@ -0,0 +1,10 @@
+import { rootReducer } from "./rootReducer"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { createStore, applyMiddleware } from "redux"
+import { logger } from "redux-logger"
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger))
+)
+store.dispatch({type: "TEST_ACTION"})
