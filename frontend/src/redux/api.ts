f546a45f03eb052fa302daefed1fd038efb249f2
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-05-11 06:17:04 -0400

    wc: work in progress

--- frontend/src/redux/api.ts
+++ frontend/src/redux/api.ts
@@ -7,7 +7,7 @@ import { RootState } from './store'
 const treeApi = createApi({
     reducerPath: 'treeApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://127.0.0.1:8000/api/v1/',
+        baseUrl: 'http://127.0.0.1:5000/api/v1/',
         prepareHeaders: (headers, { getState }) => {
             const token = (getState() as RootState).auth.token
             if (token !== 'null' && token !== null) {
